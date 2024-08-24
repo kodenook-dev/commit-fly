b4fa6572d0b636859ef74743f985c785035bd8fa
kodenook <kodenook@gmail.com>
2024-08-24 17:15:08 -0400

    feat(ui): Add a main layout :lipstick:
    Add a main layout that includes a header

---
+++ components/HeaderItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="bg-green-700 py-2 pl-4 text-white">
+        <div class="font-bold text-3xl">
+            CommitFly
+        </div>
+        <div class="italic">
+            Clear and consistent commits
+        </div>
+    </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <HeaderItem class="h-20" />
+        <main class="flex-1 bg-neutral-200 text-black">
+            <slot />
+        </main>
+    </div>
+</template>
